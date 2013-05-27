57730c0ae97479b98581ae4ac5ca4814ff24b772
bigherman <pasa@mercantec.dk>
2013-05-27 09:14:19 +0200

    Initial commit

---
+++ EmailClient/EmailClient/EmailClient/RecieveMail.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace EmailClient
+{
+    partial class RecieveMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReciveMailbtn = new System.Windows.Forms.Button();
+            this.msgcounglb = new System.Windows.Forms.Label();
+            this.Subjectlsbx = new System.Windows.Forms.ListBox();
+            this.msgBodytbx = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pgBarMailFetched = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // ReciveMailbtn
+            // 
+            this.ReciveMailbtn.Location = new System.Drawing.Point(35, 16);
+            this.ReciveMailbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ReciveMailbtn.Name = "ReciveMailbtn";
+            this.ReciveMailbtn.Size = new System.Drawing.Size(100, 28);
+            this.ReciveMailbtn.TabIndex = 0;
+            this.ReciveMailbtn.Text = "Recive";
+            this.ReciveMailbtn.UseVisualStyleBackColor = true;
+            this.ReciveMailbtn.Click += new System.EventHandler(this.ReciveMailbtn_Click);
+            // 
+            // msgcounglb
+            // 
+            this.msgcounglb.AutoSize = true;
+            this.msgcounglb.Location = new System.Drawing.Point(692, 28);
+            this.msgcounglb.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.msgcounglb.Name = "msgcounglb";
+            this.msgcounglb.Size = new System.Drawing.Size(31, 17);
+            this.msgcounglb.TabIndex = 3;
+            this.msgcounglb.Text = "N/A";
+            // 
+            // Subjectlsbx
+            // 
+            this.Subjectlsbx.FormattingEnabled = true;
+            this.Subjectlsbx.ItemHeight = 16;
+            this.Subjectlsbx.Location = new System.Drawing.Point(16, 52);
+            this.Subjectlsbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Subjectlsbx.Name = "Subjectlsbx";
+            this.Subjectlsbx.Size = new System.Drawing.Size(199, 452);
+            this.Subjectlsbx.TabIndex = 4;
+            this.Subjectlsbx.SelectedIndexChanged += new System.EventHandler(this.Subjectlsbx_SelectedIndexChanged);
+            // 
+            // msgBodytbx
+            // 
+            this.msgBodytbx.Location = new System.Drawing.Point(224, 52);
+            this.msgBodytbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.msgBodytbx.Name = "msgBodytbx";
+            this.msgBodytbx.Size = new System.Drawing.Size(552, 452);
+            this.msgBodytbx.TabIndex = 5;
+            this.msgBodytbx.Text = "";
+            // 
+            // pgBarMailFetched
+            // 
+            this.pgBarMailFetched.Location = new System.Drawing.Point(224, 528);
+            this.pgBarMailFetched.Name = "pgBarMailFetched";
+            this.pgBarMailFetched.Size = new System.Drawing.Size(552, 11);
+            this.pgBarMailFetched.TabIndex = 6;
+            // 
+            // RecieveMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 583);
+            this.Controls.Add(this.pgBarMailFetched);
+            this.Controls.Add(this.msgBodytbx);
+            this.Controls.Add(this.Subjectlsbx);
+            this.Controls.Add(this.msgcounglb);
+            this.Controls.Add(this.ReciveMailbtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RecieveMail";
+            this.Text = "RecieveMail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReciveMailbtn;
+        private System.Windows.Forms.Label msgcounglb;
+        private System.Windows.Forms.ListBox Subjectlsbx;
+        private System.Windows.Forms.RichTextBox msgBodytbx;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar pgBarMailFetched;
+    }
+}
---
+++ EmailClient/EmailClient/EmailClient/RecieveMail.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenPop.Common.Logging;
+using OpenPop.Mime;
+using OpenPop.Mime.Decode;
+using OpenPop.Mime.Header;
+using OpenPop.Pop3;
+using System.IO;
+
+namespace EmailClient
+{
+    public partial class RecieveMail : Form
+    {
+        List<OpenPop.Mime.Message> list;
+
+        private BackgroundWorker worker;
+        
+        public RecieveMail()
+        {
+            InitializeComponent();
+
+            list = new List<OpenPop.Mime.Message>();
+
+            worker = new BackgroundWorker();
+
+            worker.WorkerReportsProgress = true;
+
+            worker.DoWork += new DoWorkEventHandler(fetchAllMessages);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+        }
+
+        private void ReciveMailbtn_Click(object sender, EventArgs e)
+        {   
+            if (!worker.IsBusy)
+            {
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private static void fetchAllMessages(object sender, DoWorkEventArgs e)
+        {
+            int percentComplete;
+
+            // The client disconnects from the server when being disposed
+            using (Pop3Client client = new Pop3Client())
+            {
+                // Connect to the server
+                //client.Connect("pop.gmail.com", 995, true);
+                client.Connect("mail.server", 110, false);
+
+                // Authenticate ourselves towards the server
+                client.Authenticate("user_id", "password");
+
+                // Get the number of messages in the inbox
+                int messageCount = client.GetMessageCount();
+
+                // We want to download all messages
+                List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>(messageCount);
+
+                // Messages are numbered in the interval: [1, messageCount]
+                // Ergo: message numbers are 1-based.
+                // Most servers give the latest message the highest number
+
+                for (int i = messageCount; i > 0; i--)
+                {
+                    allMessages.Add(client.GetMessage(i));
+                    percentComplete = Convert.ToInt16((Convert.ToDouble(allMessages.Count) / Convert.ToDouble(messageCount)) * 100);
+                    (sender as BackgroundWorker).ReportProgress(percentComplete);
+                }
+
+                // Now return the fetched messages
+                e.Result = allMessages;
+            }
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgBarMailFetched.Value = e.ProgressPercentage;
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            list = (List<OpenPop.Mime.Message>)e.Result;
+
+            msgcounglb.Text = Convert.ToString(list.Count);
+
+            Subjectlsbx.Items.Clear();
+            foreach (OpenPop.Mime.Message message in list)
+            {
+                Subjectlsbx.Items.Add(message.Headers.Subject.ToString());
+            }
+        }
+
+        private void Subjectlsbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedItem = Subjectlsbx.SelectedIndex;
+            if (!list[selectedItem].MessagePart.IsMultiPart)
+            {
+                msgBodytbx.Text = list[selectedItem].MessagePart.GetBodyAsText();
+            }
+            else
+            {
+                OpenPop.Mime.MessagePart plainText = list[selectedItem].FindFirstPlainTextVersion();
+                msgBodytbx.Text = plainText.GetBodyAsText();
+            }
+        }
+    }
+}
